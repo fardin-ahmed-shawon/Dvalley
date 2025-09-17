70ea982ca89564acdcac58c82b8212193a08d467
Fardin Ahmed <fardinahmed7443@gmail.com>
2025-09-17 18:30:42 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import SectionOne from "../components/SectionOne";
+import SectionTwo from "../components/SectionTwo";
+import SectionThree from "../components/SectionThree";
+import SectionFive from "../components/SectionFive";
+import SectionFour from "../components/SectionFour";
+import SectionSix from "../components/SectionSix";
+
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <SectionOne/>
+<SectionTwo/>
+<SectionThree/>
+<SectionFour/>
+<SectionFive/>
+<SectionSix/>
+
+    </>
+  );
+};
+
+export default Home;
